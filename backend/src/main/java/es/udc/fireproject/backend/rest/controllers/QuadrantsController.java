5e1bc638f001088aa6f2732dd16e493fb7c4204f
Adrián García Vázquez <adriangvazquez@gmail.com>
2025-11-13 11:20:45 +0100

    refactor: rename endpoint files

--- backend/src/main/java/es/udc/fireproject/backend/rest/controllers/QuadrantsController.java
+++ backend/src/main/java/es/udc/fireproject/backend/rest/controllers/QuadrantsController.java
@@ -10,25 +10,23 @@ import es.udc.fireproject.backend.rest.dtos.conversors.QuadrantInfoConversor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/quadrants")
-public class QuadrantController {
+@RequiredArgsConstructor
+public class QuadrantsController implements QuadrantsApi {
 
-  @Autowired
-  FireManagementService fireManagementService;
+  private final FireManagementService fireManagementService;
 
-  @GetMapping("")
-  public List<QuadrantDto> findAll(@RequestParam(required = false) String scale) {
+  @Override
+  public ResponseEntity<List<QuadrantDto>> getQuadrants(String scale) {
 
     List<QuadrantDto> quadrantDtos = new ArrayList<>();
 
@@ -41,7 +39,7 @@ public class QuadrantController {
         quadrantDtos.add(QuadrantConversor.toQuadrantDto(quadrant));
       }
     }
-    return quadrantDtos;
+    return ResponseEntity.ok(quadrantDtos);
   }
 
   @GetMapping("/{gid}")
