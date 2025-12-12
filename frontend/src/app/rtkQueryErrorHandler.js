201b86e85d9c1563a02517f49b423f432206f439
Adrián García Vázquez <adriangvazquez@gmail.com>
2025-12-12 14:54:52 +0100

    fix: message handle

--- frontend/src/app/rtkQueryErrorHandler.js
+++ frontend/src/app/rtkQueryErrorHandler.js
@@ -2,27 +2,39 @@ import { isFulfilled, isRejected } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
 export const rtkQueryErrorLogger = (api) => (next) => (action) => {
-  if (import.meta.env.REACT_APP_MODE === "development" && isFulfilled(action)) {
+  if (import.meta.env.VITE_REACT_APP_MODE === "development" && isFulfilled(action)) {
     console.log(`Request ${action.meta.requestId} succeeded with payload:`, action.payload);
   }
 
   if (isRejected(action) && action.payload && action.error && action.error.message) {
     const { status, data } = action.payload;
 
-    if (data && data.fieldErrors) {
-      data.fieldErrors.forEach((fieldError) => {
-        const errorMessage = `${fieldError.fieldName}: ${fieldError.message}`;
-        console.error(errorMessage);
-        toast.error(errorMessage);
-      });
-    } else if (data && data.globalError) {
-      const errorMessage = data.globalError;
-      console.error(errorMessage);
-      toast.error(errorMessage);
-    } else if (status === "FETCH_ERROR") {
+    console.log(`Request ${action.meta.requestId} rejected with payload:`, action.payload);
+
+    if (status === 500) {
       console.error(action);
       toast.error("500: Internal Server Error");
+      return
     }
+
+    if (data) {
+      if (data.fieldErrors) {
+        data.fieldErrors.forEach((fieldError) => {
+          const errorMessage = `${fieldError.fieldName}: ${fieldError.message}`;
+          console.error(errorMessage);
+          toast.error(errorMessage);
+        });
+        return
+      }
+
+      if (data.errorMessage) {
+        const errorMessage = data.errorMessage;
+        console.error(errorMessage);
+        toast.error(errorMessage);
+        return
+      }
+    }
+
   }
 
   return next(action);
